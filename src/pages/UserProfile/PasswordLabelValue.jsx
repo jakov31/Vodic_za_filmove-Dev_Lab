4abbc8223bc74b3b315013a9da2141465f98377c
Jakov <jakov.janjusevic@gmail.com>
2023-10-03 12:20:15 +0200

    Header link button color changed, removed show password in UserPage

--- src/pages/UserProfile/PasswordLabelValue.jsx
+++ src/pages/UserProfile/PasswordLabelValue.jsx
@@ -1,24 +1,8 @@
-import { useState } from "react";
-
 const PasswordLabelValue = ({ value }) => {
-  const [passwordVisible, setPasswordVisible] = useState(true);
-
   return (
     <div className=" ml-5 mt-3">
       <h2 className="font-bold">Password</h2>
-      <input
-        onChange={() => {}}
-        type={passwordVisible ? "password" : ""}
-        value={value}
-        disabled={passwordVisible}
-      />
-      <button
-        onClick={() => {
-          setPasswordVisible((prev) => !prev);
-        }}
-      >
-        {passwordVisible ? "Show" : "Hide"} password
-      </button>
+      <input onChange={() => {}} type="password" value={value} />
     </div>
   );
 };
