d96ee57ba2feab8cbb019be603c14d8000a7b315
Djaponijum <predragradicevic2000@gmail.com>
2023-10-05 18:29:58 +0200

    started slider component and little modified home page

--- src/components/MovieSlider.jsx
+++ src/components/MovieSlider.jsx
@@ -1,7 +1,18 @@
-const MovieSlider = ()=>{
+const MovieSlider = ({slides})=>{
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    function handlePrevSlide (){
+        setCurrentSlide((prevSlide) =>
+        prevSlide === 0 ? slides.length - 1 : prevSlide - 1
+      );
+    };
+
+    function handleNextSlide(){
+        setCurrentSlide((prevSlide)=>(prevSlide+1)<slides.length);
+    };
 
 return <>
-    <div>
+    <div id="slider" className="">
         
 
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,6 +39,7 @@ const HomePage = () => {
 
   return (
     <div className="container mx-auto p-4">
+      <h1 className="text-center text-2xl font-semibold mb-4">Recent Movies</h1>
       <h1 className="text-center text-2xl font-semibold mb-4">Top Rated Movies</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {movies.slice(0, displayedMovies).map((movie) => (
