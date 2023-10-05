5b14d8a7d804540434692503a1d3de3f60c19452
Djaponijum <predragradicevic2000@gmail.com>
2023-10-05 15:47:31 +0200

    avoiding overwrite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ const HomePage = () => {
     <div className="bg-black">
     <div className="container mx-auto p-4">
       <h1 className="text-center text-2xl font-semibold mb-4">Top Rated Movies</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
         {movies.slice(0, displayedMovies).map((movie) => (
           <MovieCard key={movie.id} movie={movie} onClick={() => handleMovieClick(movie)}/>
         ))}
