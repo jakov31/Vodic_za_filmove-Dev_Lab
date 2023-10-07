980aaa6a9f24d5cfaf732e990719699c443c35b0
Jakov <jakov.janjusevic@gmail.com>
2023-10-07 17:08:39 +0200

    loading movie page

--- src/pages/List/Main.jsx
+++ src/pages/List/Main.jsx
@@ -5,12 +5,14 @@ import MovieCard from "../Home/MovieCard";
 import Card from "./Card";
 
 const Main = ({ filter }) => {
+  const [loading, setLoading] = useState(false);
   const [numberOfItems, setNumberOfItems] = useState(6);
 
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await getAllMovies();
         console.log(response.data);
@@ -18,6 +20,7 @@ const Main = ({ filter }) => {
       } catch (error) {
         console.error("Error fetching data:", error);
       }
+      setLoading(false);
     };
 
     fetchData();
@@ -38,6 +41,8 @@ const Main = ({ filter }) => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 justify-items-center mt-3">
+        {loading && <p className="text-center">Loading...</p>}
+
         {nameFilteredMovies.slice(0, numberOfItems).map((item) => (
           <Card key={item.id} movie={item} />
         ))}
